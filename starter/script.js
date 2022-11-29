7a9d5334db685644c00f488258a19e688111b2b4
cedagbo <cericdagbo@outlook.com>
2022-11-29 23:48:10 +0000

    updating README

--- starter/script.js
+++ starter/script.js
@@ -1,93 +1,93 @@
 // (Data already given!) Initialising password options as arrays that the user should consider along the way
 // Array of special characters to be included in password
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
+// var specialCharacters = [
+//   '@',
+//   '%',
+//   '+',
+//   '\\',
+//   '/',
+//   "'",
+//   '!',
+//   '#',
+//   '$',
+//   '^',
+//   '?',
+//   ':',
+//   ',',
+//   ')',
+//   '(',
+//   '}',
+//   '{',
+//   ']',
+//   '[',
+//   '~',
+//   '-',
+//   '_',
+//   '.'
+// ];
 
-// Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// // Array of numeric characters to be included in password
+// var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
+// // Array of lowercase characters to be included in password
+// var lowerCasedCharacters = [
+//   'a',
+//   'b',
+//   'c',
+//   'd',
+//   'e',
+//   'f',
+//   'g',
+//   'h',
+//   'i',
+//   'j',
+//   'k',
+//   'l',
+//   'm',
+//   'n',
+//   'o',
+//   'p',
+//   'q',
+//   'r',
+//   's',
+//   't',
+//   'u',
+//   'v',
+//   'w',
+//   'x',
+//   'y',
+//   'z'
+// ];
 
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
+// // Array of uppercase characters to be included in password
+// var upperCasedCharacters = [
+//   'A',
+//   'B',
+//   'C',
+//   'D',
+//   'E',
+//   'F',
+//   'G',
+//   'H',
+//   'I',
+//   'J',
+//   'K',
+//   'L',
+//   'M',
+//   'N',
+//   'O',
+//   'P',
+//   'Q',
+//   'R',
+//   'S',
+//   'T',
+//   'U',
+//   'V',
+//   'W',
+//   'X',
+//   'Y',
+//   'Z'
+// ];
 
 // Function to generate password with user input
 function generatePassword() {
@@ -174,7 +174,8 @@ function generatePassword() {
     '_',
     '.'
   ];
-
+  var possibleCharacters = [];
+  
   // Retrieving the user input and checking it throughout the process
   charactersNumber = prompt("Please, enter your password. Choose between 8 and 64 characters.");
   if (charactersNumber < 8 || charactersNumber > 64) {
@@ -225,7 +226,7 @@ function generatePassword() {
   };
 
   // Let us initialise possibleCharacters
-  var possibleCharacters = [];
+  
   if (considerLowercase) {
     possibleCharacters = possibleCharacters.concat(lowerCasedCharacters)
   }
